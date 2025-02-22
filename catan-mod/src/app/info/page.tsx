2514e89a1d50aceafcdefdcf0743ba843bcc0e07
Alex Felici <sandroms521@gmail.com>
2025-02-22 18:16:42 -0500

    Deployment Retry, WIP Options Page

---
+++ catan-mod/src/app/info/page.tsx
@@ -0,0 +1,51 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+    const [time, setTime] = useState(0)
+    const [pause, setPause] = useState(false)
+    // const [history, setHistory] = useState([])
+    const router = useRouter()
+
+    // TODO if 7, assign robber to player
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if (pause) setTime(time)
+            else if (time == 20) {
+                handleTimer()
+                setTime(0)
+            } else setTime(time + 1)
+        }, 1000)
+
+        return () => clearInterval(intervalId)
+    })
+
+    function handleTimer() {
+        router.push('/roll')
+    }
+
+    function handlePause() {
+        setPause(!pause)
+    }
+
+    return (
+        <body>
+            <div className="mainPage">
+                <div className="section">
+                    <h1>rolls</h1>
+                </div>
+                <div className="section">
+                    <h1>clock</h1>
+                    <button className="timer" onClick={handlePause}>
+                        <p className="count">{pause ? '❚❚' : time}</p>
+                    </button>
+                </div>
+                <div className="section">
+                    <h1>robber</h1>
+                </div>
+            </div>
+        </body>
+    )
+}
--- catan-mod/src/app/page.tsx
+++ catan-mod/src/app/page.tsx
@@ -1,56 +1,16 @@
-'use client'
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+'use Client'
 
-interface params {
-    playerCount: number
-    timer: number
-    players: number[]
-}
-
-export default function Home() {
-    const [time, setTime] = useState(0)
-    const [pause, setPause] = useState(false)
-    const router = useRouter()
-
-    // TODO if 7, assign robber to player
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            if (pause) setTime(time)
-            else if (time == 20) {
-                handleTimer()
-                setTime(0)
-            } else setTime(time + 1)
-        }, 1000)
-
-        return () => clearInterval(intervalId)
-    })
-
-    function handleTimer() {
-        router.push('/roll')
-    }
-
-    function handlePause() {
-        setPause(!pause)
-    }
-
+export default function Options() {
     return (
-        <body>
-            <div className="mainPage">
-                <div className="section">
-                    <h1>rolls</h1>
-                </div>
-                <div className="section">
-                    <h1>clock</h1>
-                    <button className="timer" onClick={handlePause}>
-                        <p className="count">{pause ? '❚❚' : time}</p>
-                    </button>
-                </div>
-                <div className="section">
-                    <h1>robber</h1>
-                </div>
-            </div>
-        </body>
+        <>
+            <form>
+                <label>Player Count: </label>
+                <input type="text" />
+                <br></br>
+                <label>Roll Interval: </label>
+                <input type="text" />
+            </form>
+            <div></div>
+        </>
     )
 }

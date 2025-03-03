774b4f0513a9e1415de02ae8fb4bebb82528552c
Alex Felici <sandroms521@gmail.com>
2025-03-03 13:55:04 -0500

    Add Fallback

--- catan-mod/src/app/info/layout.tsx
+++ catan-mod/src/app/info/layout.tsx
@@ -1,8 +1,9 @@
 import { Suspense } from 'react'
+
 type LayoutProps = {
     children: React.ReactNode
 }
 
 export default async function Layout({ children }: LayoutProps) {
-    return <Suspense>{children}</Suspense>
+    return <Suspense fallback={<h1>Loading</h1>}>{children}</Suspense>
 }
